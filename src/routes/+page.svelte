7c8154e97eaa4b02059374245862bc169bf61d4f
happysalada <raphael@megzari.com>
2023-03-18 10:29:54 -0400

    form: stay on same url

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,20 @@
 <script lang="ts">
   let menuOpen = false;
+  let firstName: string, lastName: string, location: string, company: string, email: string, phone: string, message: string;
+
+  let submitted = false;
+  async function contact() {
+    submitted = true;
+    const url = "https://formsubmit.co/ajax/cromano@orchidb.com";
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        'Content-Type': 'application/json', // *For JSON data, use 'application/json'. For form data, use 'application/x-www-form-urlencoded'.
+        // Add any additional headers if needed
+      },
+      body: JSON.stringify({ firstName, lastName, location, company, email, phone, message}), // Convert the JavaScript object to a JSON string
+    });    
+  }
 </script>
 <div class="bg-white">
   <header class="absolute inset-x-0 top-0 z-50">
@@ -460,7 +475,7 @@
     </p>
   </div>
   <form
-    action="https://formsubmit.co/cromano@orchidb.com"
+    action="https://formsubmit.co/"
     method="POST"
     class="mx-auto mt-16 max-w-xl sm:mt-20"
   >
@@ -476,6 +491,7 @@
             type="text"
             name="first-name"
             id="first-name"
+            bind:value={firstName}
             autocomplete="given-name"
             class="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           />
@@ -492,6 +508,7 @@
             type="text"
             name="last-name"
             id="last-name"
+            bind:value={lastName}
             autocomplete="family-name"
             class="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           />
@@ -508,6 +525,7 @@
             type="text"
             name="company"
             id="company"
+            bind:value={company}
             autocomplete="organization"
             class="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           />
@@ -524,6 +542,7 @@
             type="email"
             name="email"
             id="email"
+            bind:value={email}
             autocomplete="email"
             class="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           />
@@ -541,6 +560,7 @@
             type="text"
             name="location"
             id="location"
+            bind:value={location}
             autocomplete="organization"
             class="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           />
@@ -582,6 +602,7 @@
             type="tel"
             name="phone-number"
             id="phone-number"
+            bind:value={phone}
             autocomplete="tel"
             class="block w-full rounded-md border-0 py-2 px-3.5 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           />
@@ -598,17 +619,28 @@
             name="message"
             id="message"
             rows="4"
+            bind:value={message}
             class="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           />
         </div>
       </div>
     </div>
     <div class="mt-10">
-      <button
-        type="submit"
-        class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >Let's talk</button
-      >
+      {#if !submitted}
+        <button
+          type="button"
+          on:click={contact}
+          class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >Let's talk</button
+        >
+      {:else}
+        <button
+          type="button"
+          disabled
+          class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >Thank you! We will get back to you shortly.</button
+        >
+      {/if}
     </div>
   </form>
 </div>
